5ed063d7dcfee6c9c411df862a853d9475c218de
Ryaannn <144214893+Ryaannnbb@users.noreply.github.com>
2025-04-27 03:19:32 +0700

    add jetsream ui with livewire and ui crud admin category
    masi awalan

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,25 @@
 
 <?php
 
+use App\Http\Middleware\AdminMiddleware;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Middleware\RedirectMiddleware;
 
 Route::get('/', function () {
-    return view('admin.dashboard');
+    return view('welcome');
+});
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+    'is_admin'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+    Route::get('/test', function () {
+        return view('admin.kategori.create');
+    })->name('test');
 });
